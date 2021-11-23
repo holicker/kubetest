9b800c7f40921fab45acb15731f8d24f33edf5b0
gnehs91@nate.com <rh2112us^^>
2021-11-24 06:35:39 +0900

    kafka, zookeeper added

--- auth/src/main/java/com/spreadmat/auth/config/UserConfig.java
+++ auth/src/main/java/com/spreadmat/auth/config/UserConfig.java
@@ -11,6 +11,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 class UserConfig extends WebSecurityConfigurerAdapter {
@@ -32,7 +36,9 @@ class UserConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().configurationSource(corsConfigurationSource()).and()
                 .authorizeRequests()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll() // - (1)
                 .antMatchers("/actuator/**").permitAll()
                 .antMatchers("/member/**").permitAll()
                 .antMatchers("/oauth/member/**").permitAll()
@@ -47,4 +53,20 @@ class UserConfig extends WebSecurityConfigurerAdapter {
                 .ignoringRequestMatchers(request -> "/member/register".equals(request.getRequestURI()))
                 .ignoringRequestMatchers(request -> "/oauth/member/register".equals(request.getRequestURI()));
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        // - (3)
+        configuration.addAllowedOrigin("http://10.40.182.13:3000");
+        configuration.addAllowedOrigin("http://ae4c6f9f00c0f407297acb4d7542f902-304088184.ap-northeast-2.elb.amazonaws.com");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 }
