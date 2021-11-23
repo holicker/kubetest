52e04b51101231fdf9663451960991e7a4be495c
gnehs91@nate.com <rh2112us^^>
2021-11-24 05:37:56 +0900

    kafka, zookeeper added

--- auth/src/main/java/com/spreadmat/auth/config/UserConfig.java
+++ auth/src/main/java/com/spreadmat/auth/config/UserConfig.java
@@ -11,6 +11,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 class UserConfig extends WebSecurityConfigurerAdapter {
